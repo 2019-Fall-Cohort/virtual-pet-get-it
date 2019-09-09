8f5070d4aaa78b0d3038287b638e41d0db0d1ba7
Brad Wilson <brad.n.wilson@live.com>
2019-09-09 10:34:54 -0400

    Added startMenu method to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+const input = require('readline-sync');
 class App {
     constructor() {
         this.isOn = false;
+        this.startMenu();
     }
     startGame() {
         this.isOn = true;
@@ -8,6 +10,21 @@ class App {
     quitGame() {
         this.isOn = false;
     }
+    startMenu () {
+        let displayText = "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n" +
+        "~ Awesome Virtual Pet Amok! ~\n" +
+        " *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~";
+        console.log(displayText);
+        let startQuestion = " Start the Game?\n y/n > ";
+        let textInput = input.question(startQuestion);
+        while (!this.isOn && textInput !== "y") {
+            let startQuestion = " > "
+            let textInput = input.question(startQuestion);
+            if (textInput === "y") {
+                this.startGame();
+            }
+        }
+    }   
 }
 
 module.exports = App;
