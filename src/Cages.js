f901d60e249dbfe282990a64ce649c70e94ccf6e
Derrick Derrick_nora Nora <derrick_nora@Derricks-MacBook-Air.local>
2019-09-12 10:04:44 -0400

    cages

---
+++ src/Cages.js
@@ -0,0 +1,39 @@
+const input = require('readline-sync');
+
+class Cages { 
+    constructor(){
+        this.isDirty = true
+    }
+    cleanCage() {
+        this.isOn = false;
+    } 
+    isDirty() {
+        this.isOn = true;
+    }
+}
+
+cleanCage(cleanCageInput = "y") {
+    if (cleanCageInput === "y"){
+        this.isOn()
+        console.log("Hooray, your pet cages are clean!")
+        return;
+
+    } else if {
+        let displayText = "These pet cages sure look dirty...";
+        console.log(displayText);
+        let startQuestion = "Clean cages?\n y/n > ";
+        console.log(startQuestion);
+        break;
+        
+    else isDirty(isDirtyInput = "n") {
+        if (isDirtyInput === "n"){
+        this.isOn()
+        console.log("You monster...");
+        break;
+        }
+    }
+}
+}
+
+module.exports = Cages
+
---
+++ src/Cages.test.js
@@ -0,0 +1,23 @@
+const Cages = require('./Cages');
+
+describe("Cages", () => {
+    describe("cleanCages", () => {
+        test("cleanCages changes isOn property to 'true'", () => {
+            //Arrange
+            
+            //Act
+            
+            //Assert
+            
+        });
+    });
+    describe("isDirty", () => {   
+        test("isDirty changes isOn property to 'false'", () => {
+            //Arrange
+
+            //Act
+            
+            //Assert
+            
+        });
+    });
